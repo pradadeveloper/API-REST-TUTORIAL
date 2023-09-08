4c39b390140688d4ae2ca8533f022f2797b873c4
pradadeveloper <juanfelipepradaing@gmail.com>
2023-09-07 21:31:23 -0500

    Creación de API BÁSICA hasta acá te puedes guiar para crear una api básica

--- main.js
+++ main.js
@@ -22,24 +22,11 @@ async function reload(){
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
     const img3 = document.getElementById('img3');
-    const img4 = document.getElementById('img4');
-    const img5 = document.getElementById('img5');
-    const img6 = document.getElementById('img6');
-    const img7 = document.getElementById('img7');
-    const img8 = document.getElementById('img8');
-    const img9 = document.getElementById('img9');
-    const img10 = document.getElementById('img10');
-    
+
     img1.src = data[0].url;
     img2.src = data[1].url;
     img3.src = data[2].url;
-    img4.src = data[3].url;
-    img5.src = data[4].url;
-    img6.src = data[5].url;
-    img7.src = data[6].url;
-    img8.src = data[7].url;
-    img9.src = data[8].url;
-    img10.src = data[9].url;
+
 
 }
 
